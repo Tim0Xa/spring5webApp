054256a830dd2c9651809ce400419ae2016cf2c2
Tim0Xa <kogtim@gmial.com>
2019-01-10 12:21:22 +0200

    Creating Project on Git Hub

---
+++ src/main/java/tim/app/test/spring5webApp/Spring5webAppApplication.java
@@ -0,0 +1,14 @@
+package tim.app.test.spring5webApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Spring5webAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Spring5webAppApplication.class, args);
+	}
+
+}
+
